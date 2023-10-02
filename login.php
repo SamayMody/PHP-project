1fddd2157723d6521643dad77000f6bb7b40a6b3
Srmsamay <sm7525@srmist.edu.in>
2023-10-02 23:26:50 +0530

    Added Sessions

---
+++ login.php
@@ -0,0 +1,103 @@
+<?php
+
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+$mysqli = require __DIR__ . "/database.php";
+$sql = sprintf("SELECT * FROM user
+               WHERE username = '%s'" ,
+               $_POST["username"]);
+
+$result = $mysqli->query($sql);
+
+$user = $result->fetch_assoc();
+
+if ($user){
+    if (password_verify($_POST["password"], $user["password_hash"])) {
+        die("Login Successful");
+    }
+}
+
+$is_invalid = true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Login </title>
+
+</head>
+
+<body>
+<h1>Login</h1>
+
+<?php if ($is_invalid): ?>
+<em>Invalid credentials</em>
+<?php endif; ?>
+
+<form method="post">
+
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+
+    <button>Log In</button>
+
+</form>
+
+</body>
+</html>
